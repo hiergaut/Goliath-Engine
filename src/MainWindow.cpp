ef0f0782439b39c6d49960b01c5eefdf4c96d8f5
gauthier <gauthierbouyjou@aol.com>
2019-07-22 21:47:31 +0200

    update

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -7,23 +7,22 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-//    QSurfaceFormat format;
-//    format.setVersion(3, 3);
-//    format.setProfile(QSurfaceFormat::CoreProfile);
-//    format.setDepthBufferSize(24);
-//    format.setSamples(4);
-//    QSurfaceFormat::setDefaultFormat(format);
+    QSurfaceFormat format;
+    format.setVersion(3, 3);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    format.setDepthBufferSize(24);
+    format.setSamples(4);
+    QSurfaceFormat::setDefaultFormat(format);
 
     ui->setupUi(this);
 
-//    openglWidget = new QOpenGLWidget_World(this);
-//    openglWidget->resize(ui->centralWidget->size());
-//    openglWidget->setFocus();
+    openglWidget = new QOpenGLWidget_World(this);
+    openglWidget->resize(ui->centralWidget->size());
+    openglWidget->setFocus();
 
 //    openglWidget->resize(openglWidget->sizeHint());
 
-
-//    setCentralWidget(openglWidget);
+    setCentralWidget(openglWidget);
 
 //    ui->openGLWidget->setFocus();
 
